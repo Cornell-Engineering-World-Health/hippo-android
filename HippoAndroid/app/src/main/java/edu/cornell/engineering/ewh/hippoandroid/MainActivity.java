3a102a3a62836d36d477c49637856241d8752f1f
ec587 <ec587@cornell.edu>
2017-04-26 10:04:22 -0400

    Implement async call to retrieve sessions in MainActivity

--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
@@ -23,13 +23,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity  {
 
-    private LinearLayout publisherView;
-    private LinearLayout.LayoutParams publisherParams;
-    private LinearLayout subscriberView;
-    private LinearLayout.LayoutParams subscriberParams;
 
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
+    AsyncCall getSessions = new AsyncCall();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("Main: ", "call to onCreate");
@@ -38,55 +35,15 @@ public class MainActivity extends AppCompatActivity  {
         LinearLayout parentLayout = new LinearLayout(this);
         setContentView(parentLayout);
 
-        new getMyCalls().execute("https://ewh-hippo.herokuapp.com/api/self");
+        //this to set delegate/listener back to this class
+        getSessions.delegate = this;
+
+        //execute the async task
+        getSessions.execute("https://ewh-hippo.herokuapp.com/api/self");
     }
 
-    private class getMyCalls extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String[] params) {
-            // GET all of user's sessions
-            Log.i("getMyCalls:", "[doInBackground] params = "+params[0]);
-
-            StringBuffer response = new StringBuffer();
-
-            try {
-                URL obj = new URL(params[0]);
-                HttpURLConnection httpConnection = (HttpURLConnection) obj.openConnection();
-                httpConnection.setRequestMethod("GET");
-                httpConnection.setRequestProperty("Authorization", " Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGQ2ZmUyODFmY2RlZjAwMTFmNTU0ZjQiLCJpYXQiOjE0OTI5ODI4MTIsImV4cCI6MTQ5NDE5MjQxMn0.3scDrs3eVW53V_zboesXb2tgiyDmlvw98I8TVFgeQaY");
-                int responseCode = httpConnection.getResponseCode();
-                Log.i("getMyCalls:", ""+responseCode);
-                if (responseCode == 200) {
-                    Log.i("getMyCalls", "[doInBackground] response code = "+responseCode);
-
-                    BufferedReader responseReader = new BufferedReader(new InputStreamReader(
-                            httpConnection.getInputStream()));
-
-                    String responseLine;
-
-                    while ((responseLine = responseReader.readLine()) != null) {
-                        response.append(responseLine+"\n");
-                    }
-                    responseReader.close();
-
-                    // print result
-                    Log.i("allSessions: ", response.toString());
-                }
-            }
-            catch (MalformedURLException e) {
-                Log.d("malformed URL: ", e.getMessage());
-            }
-            catch (IOException e) {
-                Log.d("IOException", e.getMessage());
-            }
-            return response.toString();
-        }
-
-        @Override
-        protected void onPostExecute(String message) {
-            //process message
-        }
+    public void processFinish(String output) {
+        System.out.println("ProcessFinish: "+output);
     }
 
 }
