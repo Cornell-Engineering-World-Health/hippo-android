ce943d8b6c39cf17b0790777507f894296d8a9ef
ec587 <ec587@cornell.edu>
2017-05-02 17:13:33 -0400

    Change session list font to Lato and bolded title.

--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.cornell.engineering.ewh.hippoandroid;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.StringBuilderPrinter;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,6 +18,9 @@ import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse {
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         TextView mTitle = (TextView) myToolbar.findViewById(R.id.toolbar_title);
-        Typeface lato = Typeface.createFromAsset(this.getApplication().getAssets(), "fonts/Lato-Regular.ttf");
+        Typeface lato = Typeface.createFromAsset(this.getApplication().getAssets(), "fonts/Lato-Bold.ttf");
         mTitle.setTypeface(lato);
 
         //this to set delegate/listener back to this class
@@ -73,8 +78,8 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             }
 
             try {
-                ArrayAdapter<CallSession> sessionsAdapter;
-                sessionsAdapter = new ArrayAdapter<CallSession>(getApplicationContext(), R.layout.session_item, sessions);
+                SessionAdapter<CallSession> sessionsAdapter;
+                sessionsAdapter = new SessionAdapter<CallSession>(getApplicationContext(), R.layout.session_item, sessions);
 
                 ListView sessionListView = (ListView) findViewById(R.id.session_list);
                 sessionListView.setAdapter(sessionsAdapter);
@@ -93,7 +98,6 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
                         startActivity(intent);
                     }
                 });
-
             }
             catch(Exception e) {
                 Log.d("processFinish", "Error while creating list view: "+e);
@@ -102,7 +106,6 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         } catch(Exception e) {
             Log.d("processFinish", "Error while processing JSON: "+e.getMessage());
         }
-
 
     }
 
