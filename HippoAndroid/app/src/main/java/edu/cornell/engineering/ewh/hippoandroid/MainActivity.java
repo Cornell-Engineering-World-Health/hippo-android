b4aa6f7cffdd20959049da24531eb0cd15266340
Sonia Appasamy <sma246@cornell.edu>
2017-05-03 23:02:48 -0400

    fixed the screen proportions

--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/MainActivity.java
@@ -34,6 +34,14 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/VideoCallActivity.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/VideoCallActivity.java
@@ -1,6 +1,8 @@
 package edu.cornell.engineering.ewh.hippoandroid;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
 import android.util.DisplayMetrics;
 
+import com.opentok.android.BaseVideoRenderer;
 import com.opentok.android.Publisher;
 import com.opentok.android.PublisherKit;
 import com.opentok.android.Session;
@@ -24,6 +27,7 @@ import com.opentok.android.Stream;
 import com.opentok.android.Subscriber;
 import com.opentok.android.SubscriberKit;
 import com.opentok.android.OpentokError;
+import com.opentok.android.VideoUtils;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -54,6 +58,14 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         Log.i(LOGTAG, "call to onCreate");
         super.onCreate(savedInstanceState);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         Intent intent = getIntent();
         String sessionName = intent.getStringExtra(MainActivity.SESSION_NAME);
 
@@ -62,8 +74,8 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         height = metrics.heightPixels;
         width = metrics.widthPixels;
-        button_height = (int) 0.167 * height;
-        button_width = (int) 0.333 * width;
+        button_height = (int) height/7;//0.167 * height;
+        button_width = (int) width/3;//0.333 * width;
 
         Log.i(LOGTAG, "height : " + height);
         Log.i(LOGTAG, "width : " + width);
@@ -127,12 +139,16 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         Log.i(LOGTAG, "call to onConnected of the SessionListener");
         final Publisher publisher = new Publisher(VideoCallActivity.this);
         publisher.setPublisherListener(this);
+        publisher.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,BaseVideoRenderer.STYLE_VIDEO_FILL);
         publisherView.addView(publisher.getView());
+        //publisherView.setLayoutParams(new RelativeLayout.LayoutParams(width,height));
         session.publish(publisher);
+        Resources res = getResources();
 
         ToggleButton toggleVideo = new ToggleButton(this);
         toggleVideo.setChecked(true);
         toggleVideo.setText("Video Off");
+        toggleVideo.setButtonDrawable(res.getDrawable(android.R.drawable.ic_menu_camera));
         toggleVideo.setTextOn("Video Off");
         toggleVideo.setTextOff("Video On");
         toggleVideo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -158,6 +174,7 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         toggleAudio.setText("Mute");
         toggleAudio.setTextOn("Mute");
         toggleAudio.setTextOff("Unmute");
+        toggleAudio.setButtonDrawable(res.getDrawable(android.R.drawable.ic_btn_speak_now));
         toggleAudio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -175,7 +192,8 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         buttonView.addView(toggleAudio);
 
         Button endCall = new Button(this);
-        endCall.setText("End Call");
+        //endCall.setText("End Call");
+        endCall.setBackground(res.getDrawable(android.R.drawable.ic_menu_call));
         endCall.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 publisher.destroy();
@@ -194,6 +212,8 @@ public class VideoCallActivity extends AppCompatActivity implements Session.Sess
         Log.i(LOGTAG, "call to onStreamReceived");
         Subscriber subscriber = new Subscriber(VideoCallActivity.this, stream);
         subscriber.setVideoListener(this);
+        subscriber.setPreferredResolution(new VideoUtils.Size(width,height));
+        subscriber.setStyle(BaseVideoRenderer.STYLE_VIDEO_SCALE,BaseVideoRenderer.STYLE_VIDEO_FILL);
         session.subscribe(subscriber);
         subscriberView.addView(subscriber.getView());
     }
