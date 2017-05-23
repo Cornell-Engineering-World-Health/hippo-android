8c4e7e8585f90d7ea2c9bf013866a1257f3831de
Erin Chen <ec587@users.noreply.github.com>
2017-05-23 15:36:37 -0400

    Update AsyncCall.java
    documentation + cleaning

--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/AsyncCall.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/AsyncCall.java
@@ -13,7 +13,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Created by erinchen on 4/26/17.
+ * Module that executes any HTTP call asynchronously.
+ * Used with AsyncResponse to retrieve call result.
  */
 
 public class AsyncCall extends AsyncTask<String, Void, String> {
@@ -21,8 +22,6 @@ public class AsyncCall extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String[] params) {
-        // GET all of user's sessions
-        Log.i("getMyCalls:", "[doInBackground] params = "+params[0]);
 
         StringBuffer response = new StringBuffer();
 
@@ -33,10 +32,9 @@ public class AsyncCall extends AsyncTask<String, Void, String> {
             httpConnection.setRequestMethod("GET");
             httpConnection.setRequestProperty("Authorization", token);
             int responseCode = httpConnection.getResponseCode();
-            Log.i("getMyCalls:", ""+responseCode);
+            Log.i("AsyncCall", "[doInBackground] response code = "+responseCode);
+            
             if (responseCode == 200) {
-                Log.i("getMyCalls", "[doInBackground] response code = "+responseCode);
-
                 BufferedReader responseReader = new BufferedReader(new InputStreamReader(
                         httpConnection.getInputStream()));
 
@@ -46,8 +44,6 @@ public class AsyncCall extends AsyncTask<String, Void, String> {
                     response.append(responseLine+"\n");
                 }
                 responseReader.close();
-                // print result
-                Log.i("allSessions: ", response.toString());
             }
         }
         catch (MalformedURLException e) {
@@ -62,8 +58,8 @@ public class AsyncCall extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPostExecute(String message) {
-        //process message
-        System.out.println("onPostExecute: Got message!");
+        // process message
+        // System.out.println("onPostExecute: Got message!");
         delegate.processFinish(message);
     }
 }
