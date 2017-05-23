44f9a22861f7f001a70d7ab83d022270b2637065
Erin Chen <ec587@users.noreply.github.com>
2017-05-23 15:37:29 -0400

    Update SessionAdapter.java
    documentation

--- HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/SessionAdapter.java
+++ HippoAndroid/app/src/main/java/edu/cornell/engineering/ewh/hippoandroid/SessionAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import java.util.List;
 
 /**
- * Created by erinchen on 5/2/17.
+ * Customizes upcoming session list font.
  */
 
 public class SessionAdapter<CallSession> extends ArrayAdapter<CallSession> {
